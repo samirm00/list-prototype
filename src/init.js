40098ef68695474b034b84dae03a93e3f38f1283
samirm00 <sammoham.be@gmail.com>
2021-01-24 01:03:16 +0100

    create a branch 3-instance

--- src/init.js
+++ src/init.js
@@ -1,1 +1,8 @@
 
+"use strict";
+
+import { createNewListHandler } from "./handlers/create-new-list.js";
+
+document
+  .getElementById("list-name-input")
+  .addEventListener("keyup", createNewListHandler);
